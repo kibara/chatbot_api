7079fb3416e0b656c4aad359cdddc9450cefc22b
kibara <33486148+kibara@users.noreply.github.com>
2017-11-15 20:21:17 +0900

    up

--- dbaccess.py
+++ dbaccess.py
@@ -4,13 +4,13 @@ import psycopg2
 
 
 def postquery(text):
-    connection = psycopg2.connect("host=localhost dbname=chatbot_api user=postgres")
+    connection = psycopg2.connect("host=localhost dbname=chatbot_api user=postgres password=branbran")
     cursor = connection.cursor()
     cursor.execute("select * from messages where tag1 LIKE '%%%s%%' or tag2 LIKE '%%%s%%' or tag3 LIKE '%%%s%%'" % (text, text, text))
     res = cursor.fetchall()
 
     if len(res) == 0:
-        cursor.execute("select * from sample where id='ng001'")
+        cursor.execute("select * from messages where id='ng001'")
         res = cursor.fetchall()
 
     cursor.close()
